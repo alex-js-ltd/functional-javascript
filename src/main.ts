272eb569057c1942726e58e6f5afebc4e013ebf5
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-01 20:36:19 +0100

    update folders

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
-import { addTwo } from './challenge_1'
-import { addS } from './challenge_2'
-import { objOfMatches } from './challenge_9'
-import { multiMap } from './challenge_10'
-import { commutative } from './challenge_11'
-import { highestFunc } from './challenge_15'
-
-export { highestFunc }
+console.log('npm run test 🚀')
