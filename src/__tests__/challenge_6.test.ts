f0ca27d61d7dada79bfe6fa646dd42d829153659
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-01 12:02:45 +0100

    update tests

---
+++ src/__tests__/challenge_6.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+
+import { reduce } from '../challenge_6'
+
+test('reduce', () => {
+	const nums = [4, 1, 3]
+	const add = (a: number, b: number) => a + b
+
+	expect(reduce(nums, add, 0)).toBe(8)
+})
