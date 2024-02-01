830d2df51c72b3249dec7fb6ba2a232159d03171
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-01 23:57:58 +0100

    update

--- src/Higher-Order_Functions/challenge_1.ts
+++ src/Higher-Order_Functions/challenge_1.ts
@@ -1,4 +1,6 @@
 // Challenge 1
+// Create a function addTwo that accepts one input and adds 2 to it.
+
 export const addTwo = (num: number) => {
 	return num + 2
 }
--- src/Higher-Order_Functions/challenge_2.ts
+++ src/Higher-Order_Functions/challenge_2.ts
@@ -1,4 +1,6 @@
 // Challenge 2
+// Create a function addS that accepts one input and adds an "s" to it.
+
 export const addS = (word: string) => {
 	return word + 's'
 }
