df8e4cd6f05a6881eccbfbf6e639b923d71fd8dc
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-02 12:39:01 +0100

    update chall 7

---
+++ src/Closure/challenge_7.ts
@@ -0,0 +1,12 @@
+// Challenge 7
+// Write a function delay that accepts a callback as the first parameter and the wait in milliseconds before allowing the callback to be invoked as the second parameter. Any additional arguments after wait are provided to func when it is invoked. HINT: research setTimeout();
+
+// Challenge 7
+
+const delay = <Args extends unknown[]>(
+	func: (...args: Args) => void,
+	wait: number,
+	...args: Args
+) => {
+	setTimeout(() => func(...args), wait)
+}
