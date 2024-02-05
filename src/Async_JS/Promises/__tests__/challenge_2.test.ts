bc6c1261064ac02b8343da79b1832e3e4807421d
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-05 18:09:50 +0100

    update tests

--- src/Async_JS/Promises/__tests__/challenge_2.test.ts
+++ src/Async_JS/Promises/__tests__/challenge_2.test.ts
@@ -1,21 +1,11 @@
-import { expect, test, vi, beforeEach, afterEach } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { promise } from '../challenge_2'
-
-beforeEach(() => {
-	vi.useFakeTimers({ shouldAdvanceTime: true })
-})
-
-afterEach(() => {
-	vi.runOnlyPendingTimers()
-	vi.useRealTimers()
-})
 
 test('promise', async () => {
 	const logSpy = vi.spyOn(global.console, 'log')
 
 	await expect(promise).resolves.toBe('Resolved!')
-
-	vi.advanceTimersByTime(1000)
+	await promise
 
 	expect(logSpy).toHaveBeenCalledWith('Resolved!')
 })
