f933c1981b5248d975de352a713de9071ffc957b
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-05 13:45:46 +0100

    update

---
+++ src/Extension_Challenges/__tests__/challenge_5.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from 'vitest'
+import { curriedAddThreeNums } from '../challenge_5'
+
+test('curriedAddThreeNums', () => {
+	expect(curriedAddThreeNums(3)(-1)(1)).toEqual(3) // should log 3
+
+	expect(curriedAddThreeNums(2)(3)(5)).toEqual(10) // should log 10
+})
