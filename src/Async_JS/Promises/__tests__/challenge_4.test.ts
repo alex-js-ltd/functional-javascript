c61c24968283525e52831875d12939df8c93bc8a
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-05 17:29:13 +0100

    update test for challenge 4

--- src/Async_JS/Promises/__tests__/challenge_4.test.ts
+++ src/Async_JS/Promises/__tests__/challenge_4.test.ts
@@ -4,13 +4,11 @@ import { promise } from '../challenge_4'
 test('challenge_4', async () => {
 	const logSpy = vi.spyOn(global.console, 'log')
 
-	let array: string[] = []
-
 	await expect(promise).resolves.toBe('Promise has been resolved!')
-	promise.then(res => array.push(res))
-	array.push("I'm not the promise!")
+	promise.then(res => console.log(res))
+	console.log("I'm not the promise!")
 	await promise
-	array.forEach(el => console.log(el))
+
 	expect(logSpy).toHaveBeenCalledWith("I'm not the promise!")
 	expect(logSpy).toHaveBeenCalledWith('Promise has been resolved!')
 })
